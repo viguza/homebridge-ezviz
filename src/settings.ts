f2055f6f0af13653597a05ba3ced4ab1cccca7f7
Victor Guzman <vguzmana89@gmail.com>
2025-01-08 13:10:09 -0600

    Rename plugin

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'EZVIZ';
 /**
  * This must match the name of your plugin as defined the package.json `name` property
  */
-export const PLUGIN_NAME = 'homebridge-ezviz';
+export const PLUGIN_NAME = '@viguza/homebridge-ezviz';
